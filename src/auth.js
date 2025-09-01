085ed02662f0722f1a8b171297995fcb243953ef
Siyabonga Samson Lukhele <siyabonga.lukhele@umuzi.org>
2025-09-01 18:06:40 +0200

    feat: Add authentication functions for user registration and login

---
+++ src/auth.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export const register = async (email, username, password) => {
+  try {
+    const response = await axios.post(`${apiUrl}/api/register`, {
+      email,
+      username,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw error;
+  }
+};
+
+export const login = async (identifier, password) => {
+  try {
+    const response = await axios.post(`${apiUrl}/api/login`, {
+      identifier,
+      password,
+    });
+    localStorage.setItem("token", response.data.token);
+    return response.data;
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    throw error;
+  }
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+};
+
+export const isCurrentUserLoggedIn = () => {
+  return !!localStorage.getItem("token");
+};
