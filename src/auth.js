4c835f09cb9077bb751e4f63569413fc960396c8
Siyabonga Samson Lukhele <siyabonga.lukhele@umuzi.org>
2025-09-02 09:34:58 +0200

    feat: Update register and login functions to include fullName and use both email and username as identifier

--- src/auth.js
+++ src/auth.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { getToken } from "./utils/getToken";
 const apiUrl = import.meta.env.VITE_API_URL;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-export const register = async (email, username, password) => {
+export const register = async (email, username, password, fullName) => {
   try {
     const response = await axios.post(`${apiUrl}/api/register`, {
       email,
       username,
       password,
+      fullName,
     });
     return response.data;
   } catch (error) {
@@ -16,10 +18,11 @@ export const register = async (email, username, password) => {
   }
 };
 
-export const login = async (identifier, password) => {
+export const login = async (email, username, password) => {
   try {
     const response = await axios.post(`${apiUrl}/api/login`, {
-      identifier,
+      email,
+      username,
       password,
     });
     localStorage.setItem("token", response.data.token);
